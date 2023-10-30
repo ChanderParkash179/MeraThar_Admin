3e2e9b0d362def4a58c500c98db8e4e2d2368a8c
Chander Parkash <chanderparkash179@gmail.com>
2023-10-30 20:09:44 +0500

    updated base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // BASE_URL: 'http://localhost:8084/api/mera_thar'
-  BASE_URL: 'https://meratharbackend-production.up.railway.app/api/mera_thar'
+  BASE_URL: 'http://localhost:8084/api/mera_thar'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // BASE_URL: 'http://localhost:8084/api/mera_thar/city/'
-  BASE_URL: 'https://meratharbackend-production.up.railway.app/api/mera_thar'
+  BASE_URL: 'http://localhost:8084/api/mera_thar/city/'
 }
